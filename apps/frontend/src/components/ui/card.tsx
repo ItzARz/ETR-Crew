8e1287742c77952338d0f507ed357d14e2361dec
Albert R <rojasalbert15@gmail.com>
2024-12-22 01:23:01 -0400

    fix: linter and added shadcn chart component

--- apps/frontend/src/components/ui/card.tsx
+++ apps/frontend/src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl bg-card text-card-foreground shadow-custom-box",
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
