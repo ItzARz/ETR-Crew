5575e83718b0a919ee67b082bf3946346f97fc6b
Albert R <rojasalbert15@gmail.com>
2024-12-23 03:12:35 -0400

    added basic login page

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './dashboard/page.tsx'
+import Login from './login/page.tsx'
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </ThemeProvider>
   )
